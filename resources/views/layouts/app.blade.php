b60b6063bd62bbff78f02ee09fdbace69a0df449
lishuoz <zhanglishuo13@gmail.com>
2017-05-02 16:25:43 -0400

    Add no follow meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="robots" content="noindex,nofollow">
 
   <title>{{ config('app.name', 'Laravel') }}</title>
 
