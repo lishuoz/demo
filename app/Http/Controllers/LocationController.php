74106c38fe93889bf7b01faccd65c48193f87135
lishuoz <zhanglishuo13@gmail.com>
2017-05-08 16:20:24 -0400

    Fix bug in LocationController

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -16,7 +16,6 @@ class LocationController extends Controller
 			$origin = $request->session()->get('origin');
 		}
 
-		dd($request->ip);
 		// Get GEO Location of the user
 		$location = \Location::get($request->ip);
 
